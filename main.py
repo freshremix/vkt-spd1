292682c3dfe55a3c123cf24407b48f661b249210
freshremix <90133873+freshremix@users.noreply.github.com>
2022-10-06 15:25:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def get_single_song(bot, update):
     bot.send_message(chat_id=chat_id, text="Fetching...")
 
     if config["SPOTDL_DOWNLOADER"]:
-        os.system(f'spotdl --audio youtube-music {url} --st 10 --dt 25')
+        os.system(f'spotdl {url} --st 10 --dt 25')
     elif config["SPOTIFYDL_DOWNLOADER"]:
         os.system(f'spotifydl {url}')
     else:
