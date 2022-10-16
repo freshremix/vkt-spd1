b7bffeb0dc60d842c63fef886bdef0d48e1c247f
freshremix <90133873+freshremix@users.noreply.github.com>
2022-10-16 19:05:48 +0330

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ except:
 updater = Updater(token, use_context=True)
 dispatcher = updater.dispatcher
 
-def get_single_song_handler(update: Update, context: Bot.CallbackContext):
+def get_single_song_handler(update: telegram.Update, context: telegram.ext.CallbackContext):
     if config["AUTH"]["ENABLE"]:
         authenticate(context, update)
     get_single_song(context, update)
 
 
-def get_single_song(update: Update, context: Bot.CallbackContext):
+def get_single_song(update: telegram.Update, context: telegram.ext.CallbackContext):
     chat_id = update.effective_message.chat_id
     message_id = update.effective_message.message_id
     username = update.message.chat.username
