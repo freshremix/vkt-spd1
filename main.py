c6ecaa9730ee38cf5fcb95b04477efcbd6ce7e2c
freshremix <90133873+freshremix@users.noreply.github.com>
2022-10-16 16:48:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ except:
 updater = Updater(token, use_context=True)
 dispatcher = updater.dispatcher
 
-def get_single_song_handler(`update: Update, context: CallbackContext`):
+def get_single_song_handler(update: Update, context: CallbackContext):
     if config["AUTH"]["ENABLE"]:
         authenticate(update, bot)
     get_single_song(update, bot)
 
 
-def get_single_song(`update: Update, context: CallbackContext`):
+def get_single_song(update: Update, context: CallbackContext):
     chat_id = update.effective_message.chat_id
     message_id = update.effective_message.message_id
     username = update.message.chat.username
@@ -76,7 +76,7 @@ def get_single_song(`update: Update, context: CallbackContext`):
 
 
 
-def authenticate(`update: Update, context: CallbackContext`):
+def authenticate(update: Update, context: CallbackContext):
     username = update.message.chat.username
     chat_id = update.effective_message.chat_id
     if update.effective_message.text == config["AUTH"]["PASSWORD"]:
