93ff1355942331c6acdde66b4053fc22b448fd72
freshremix <90133873+freshremix@users.noreply.github.com>
2022-10-16 18:20:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def get_single_song(update: Update, context: Bot):
 
 
 
-def authenticate(update: Update, context: Bot):
+def authenticate(context, update):
     username = update.message.chat.username
     chat_id = update.effective_message.chat_id
     if update.effective_message.text == config["AUTH"]["PASSWORD"]:
