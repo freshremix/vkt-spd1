8ceb37f4098ced574a3f868fe19698335254440d
freshremix <90133873+freshremix@users.noreply.github.com>
2022-10-26 13:39:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def get_single_song(bot, update):
         bot.send_message(chat_id=chat_id, text="Sending to You...")
         files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(".") for f in filenames if os.path.splitext(f)[1] == '.mp3']
         for file in files:
-            bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb', timeout=16000))
+            bot.send_audio(chat_id=chat_id, audio=open(f'./{file}', 'rb'), timeout=15000)
             sent += 1
     except:
         pass
@@ -92,6 +92,6 @@ def authenticate(bot, update):
 handler = MessageHandler(Filters.text, get_single_song_handler)
 dispatcher.add_handler(handler=handler)
 
-POLLING_INTERVAL = 8.8
+POLLING_INTERVAL = 0.3
 updater.start_polling(poll_interval=POLLING_INTERVAL)
 updater.idle()
