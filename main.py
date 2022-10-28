6fd2a0f1d87395a1254a32a9d497b2edfd1dcd00
freshremix <90133873+freshremix@users.noreply.github.com>
2022-10-28 18:43:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -94,6 +94,6 @@ def authenticate(bot, update):
 handler = MessageHandler(Filters.text, get_single_song_handler)
 dispatcher.add_handler(handler=handler)
 
-POLLING_INTERVAL = 8.8
+POLLING_INTERVAL = 0.3
 updater.start_polling(poll_interval=POLLING_INTERVAL)
 updater.idle()
